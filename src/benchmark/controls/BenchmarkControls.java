dc739edd8b673dd02e85d739fa9ea99449f4df38
tomwang2011 <tong.wang@liferay.com>
2016-03-24 16:04:15 -0700

    Remove the default starting screen

--- src/benchmark/controls/BenchmarkControls.java
+++ src/benchmark/controls/BenchmarkControls.java
@@ -10,11 +10,6 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -26,31 +21,17 @@ public class BenchmarkControls extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		_primaryStage = primaryStage;
-		Button btn = new Button();
-		btn.setText("Say 'Hello World'");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
 
-			@Override
-			public void handle(ActionEvent event) {
-				MainControlPanelController controller = new MainControlPanelController();
+		MainControlPanelController controller =
+			new MainControlPanelController();
 
-				try {
-					controller.showStage();
-				} catch (IOException ex) {
-					Logger.getLogger(
-						BenchmarkControls.class.getName()).log(Level.SEVERE, null, ex);
-				}
-			}
-		});
-
-		StackPane root = new StackPane();
-		root.getChildren().add(btn);
-
-		Scene scene = new Scene(root, 1000, 800);
-
-		primaryStage.setTitle("Hello World!");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		try {
+			controller.showStage();
+		}
+		catch (IOException ex) {
+			Logger.getLogger(
+				BenchmarkControls.class.getName()).log(Level.SEVERE, null, ex);
+		}
 	}
 
 	/**
